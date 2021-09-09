a10531282ca11ac28c3074dbc85c60437b92337f
Chugh <parchugh@88665a14c197.ant.amazon.com>
2021-09-09 17:12:52 +0800

    Added Context Var

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -7,7 +7,7 @@ class TestHandlerCase(unittest.TestCase):
     def test_response(self):
         print("testing response.")
         print(eventobj)
-        result = index.lambda_handler(eventobj)
+        result = index.lambda_handler(eventobj,{})
         print(result)
         self.assertEqual(result['statusCode'], 200)
         self.assertEqual(result['headers']['Content-Type'], 'application/json')
